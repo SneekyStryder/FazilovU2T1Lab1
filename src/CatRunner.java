b7ffd84dc8499c516cdb769b5a9b76859c92f0c0
Imran Fazilov <ifazilov3584@bths.edu>
2022-09-29 12:28:44 -0400

    almost done with the lab

---
+++ src/CatRunner.java
@@ -0,0 +1,14 @@
+public class CatRunner {
+    public static void main(String[] args)
+    {
+        Cat cat1 = new Cat("Stryder", 3, 7);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Dino", 4, 8.2);
+        cat2.introduce();
+        cat2.printCatInfo();
+
+    }
+}
+
